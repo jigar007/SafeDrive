9bc48da730dd941b154499d8308cc59d1b7aca53
Haoran Sun <254664427@qq.com>
2017-10-07 12:09:05 +1100

    test commit

--- app/src/main/java/com/unimelb/jigarthakkar/safedrivesystem/MainActivity.java
+++ app/src/main/java/com/unimelb/jigarthakkar/safedrivesystem/MainActivity.java
@@ -127,6 +127,7 @@ public class MainActivity extends FragmentActivity
         locationRequest.setFastestInterval(1000);
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
+    ///
 
 
     @Override
